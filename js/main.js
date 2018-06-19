307cf5fbd59c5abe0f9de1993d3709aa40386879
dengdaisisuo <dengdaisisuo@gmail.com>
2018-06-19 10:56:28 +0800

    1
    111

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-import index from './index'
-var vm = new Vue({
-	
-  // 选项
-   el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
+window.onload=function(){
+				var vm = new Vue({
+				// 选项
+				el: '#app',
+				data: {
+				  message: 'Hello Vuedsds!'
+				},
+				
+				methods:{
+					reverseMessage:function(){
+						this.message = this.message.split('').reverse().join('')
+					}
+				}
+			
+				
+				
+			})
+			}
